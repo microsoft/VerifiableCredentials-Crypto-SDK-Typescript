ecfe50173146dba5cca85d59866fbfa00038bac6
Ronny Bjones <ronnybj@microsoft.com>
2020-07-22 19:57:07 +0200

    update unit test

--- libs/keyStore/tests/KeyReference.spec.ts
+++ libs/keyStore/tests/KeyReference.spec.ts
@@ -4,7 +4,7 @@ import { CryptoKey } from 'webcrypto-core';
 describe('KeyReference', () =>{
     it('should create a KeyReference', () =>{
         let reference = new KeyReference('key');
-        expect(reference.type).toEqual('key');
+        expect(reference.type).toEqual('secret');
         expect(reference.keyReference).toEqual('key');
         expect(reference.cryptoKey).toBeUndefined();
 
